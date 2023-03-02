f64068903cd465a00d717985b31e8814dcf8556a
phiwe saba <sabasbulele81@gmail.com>
2023-03-02 20:32:21 +0200

    working on logic to calculate scores...

--- script.js
+++ script.js
@@ -13,22 +13,39 @@ function getPlayerChoice(){
 
 function getChoices(getComputerChoice, getPlayerChoice){
 
+    let playerScore = 0, playerTotal = 0;
+    let computerScore = 0, computerTotal = 0;
+
     if(getComputerChoice == getPlayerChoice){
         console.log("Tie");
     }else{
         if(getComputerChoice == "rock" && getPlayerChoice == "paper"){
+            playerScore++;
             return getPlayerChoice;
             //console.log(getPlayerChoice + " beats " + getComputerChoice);
         }else if(getComputerChoice == "paper" && getPlayerChoice == "scissors"){
+            playerScore++;
             return getPlayerChoice;
             //console.log(getPlayerChoice + " beats " + getComputerChoice);
-        }else if(getComputerChoice == "scissors" && getPlayerChoice == "rock")
-        {
+        }else if(getComputerChoice == "scissors" && getPlayerChoice == "rock"){
+            playerScore++;
             return getPlayerChoice;
             //console.log(getPlayerChoice + " beats " + getComputerChoice);
         }else{
+            computerScore++;
             console.log("Computer wins");
         }
+    }
+
+    playerTotal = playerTotal + playerScore;
+    computerTotal = computerTotal + computerScore;
+
+    if(playerTotal = computerTotal){
+        console.log("It's a tie! " + playerTotal);
+    }else if(playerTotal > computerTotal){
+        console.log("Player wins with " + playerTotal);
+    }else{
+        console.log("Computer wins with " + computerTotal);
     }
 }
 
